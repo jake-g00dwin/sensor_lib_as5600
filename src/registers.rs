dcb5f2d0a402973f5f3735a30d4dc4eee92333e2
jakeg00dwin <jakegoodwin@gorge.works>
2024-08-30 14:00:18 -0700

    defined bit maskss for the configuration register.

--- src/registers.rs
+++ src/registers.rs
@@ -43,4 +43,71 @@ pub enum BurnCommands{
     Burn = 0xFF,
 }
 
+#[repr(u16)]
+#[allow(dead_code)]
+pub enum PowerModeBM{
+    Nom = 0x0000,
+    LPM1 = 0x0001,
+    LPM2 = 0x0002,
+    LPM3 = 0x0003,
+}
+
+
+#[repr(u16)]
+#[allow(dead_code)]
+pub enum HysteresisBM{
+    HysteresisOff = 0x0000,
+    HysteresisLSB1 = 0x0000 | (1<<2),
+    HysteresisLSB2 = 0x0000 | (1<<3),
+    HysteresisLSB3 = 0x0000 | (1<<2)|(1<<3),
+}
+
+#[repr(u16)]
+#[allow(dead_code)]
+pub enum OutputStageBM{
+    VDD = 0,
+    VDD1 = (1<<4),
+    PWM = (1<<5),
+}
+
+
+#[repr(u16)]
+#[allow(dead_code)]
+pub enum PWMFrequencyBM{
+    F115hz = 0,
+    F230hz = (1<<6),
+    F460hz = (1<<7),
+    F920hz = (1<<7)|(1<<6),
+}
+
+
+#[repr(u16)]
+#[allow(dead_code)]
+pub enum SlowFilterBM{
+    Fltr16x = 0,
+    Fltr8x = (1<<8),
+    Fltr4x = (1<<9),
+    Fltr2x = (1<<9)|(1<<8),
+}
+
+#[repr(u16)]
+#[allow(dead_code)]
+pub enum FastFilterThresholdBM{
+    SlowOnly = 0,
+    LSB6 = (1<<10),
+    LSB7 = (1<<11),
+    LSB9 = (1<<11)|(1<<10),
+    LSB18 = (1<<12),
+    LSB21 = (1<<12)|(1<<10),
+    LSB24 = (1<<12)|(1<<11),
+    LSB10 = (1<<12)|(1<<11)|(1<<10),
+}
+
+#[repr(u16)]
+#[allow(dead_code)]
+pub enum WatchDogBM{
+    Off = 0,
+    On = (1<<13),
+}
+
 
